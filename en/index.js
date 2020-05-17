b6feec8de3f112f647e1257eb34442c815a50ee7
marciojrtorres <marciojrtorres@gmail.com>
2020-05-16 21:07:33 -0300

    game

---
+++ en/index.js
@@ -0,0 +1,55 @@
+import { idioms } from './idioms.js';
+
+const idiom = document.querySelector('#idiom');
+const meaning = document.querySelector('#meaning');
+const buttons = document.querySelector('footer');
+
+const ok = document.querySelector('button.ok');
+const no = document.querySelector('button.no');
+const meh = document.querySelector('button.meh');
+
+let list;
+let counter = 0;
+
+document.addEventListener('DOMContentLoaded', init);
+
+function init() {
+    ok.addEventListener('click', (e) => {
+        buttons.style.display = 'none';
+        meaning.style.height = '0';
+        meaning.style.display = 'none';
+        idiom.style.height = '100%';
+        register();
+        nextIdiom();
+    });
+
+    idiom.addEventListener('click', (e) => {
+        meaning.style.height = '67%';
+        meaning.style.display = 'flex';
+        meaning.style.color = 'black';
+        idiom.style.height = '33%';
+        buttons.style.display = 'block';
+    });
+
+    const store = localStorage.getItem('idioms');
+    if (store) {
+        idioms = JSON.parse(store);
+    }
+    list = Object.keys(idioms);
+    const len = list.length;
+    for (let i = 0; i < len; i++) {
+        const o = parseInt(Math.random() * len);
+        [list[i], list[o]] = [list[o], list[i]];
+    }    
+    nextIdiom();
+}
+
+function nextIdiom() {
+    idiom.textContent = list[counter];
+    meaning.textContent = idioms[list[counter]];
+    counter++;
+}
+
+function register() {
+    
+}
